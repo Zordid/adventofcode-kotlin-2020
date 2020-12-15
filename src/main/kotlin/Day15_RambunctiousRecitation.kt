b63e7b75010123035f78f791f9c8691d935e6dc7
Olaf Gottschalk <Zordid@users.noreply.github.com>
2020-12-15 07:39:44 +0100

    add Day 15: Rambunctious Recitation

---
+++ src/main/kotlin/Day15_RambunctiousRecitation.kt
@@ -0,0 +1,24 @@
+class Day15 : Day(15, title = "Rambunctious Recitation") {
+
+    private val numbers = parsedInput { ints }[0]
+
+    override fun part1() = game(numbers, 2020)
+
+    override fun part2() = game(numbers, 30000000)
+
+    private fun game(startNumbers: List<Int>, n: Int): Int {
+        val stats = mutableMapOf<Int, Int>()
+
+        startNumbers.dropLast(1).forEachIndexed { index, i -> stats[i] = index }
+
+        return (startNumbers.size - 1 until n - 1).fold(startNumbers.last()) { lastSpoken, index ->
+            val lastSeen = stats[lastSpoken]
+            stats[lastSpoken] = index
+            lastSeen?.let { index - it } ?: 0
+        }
+    }
+}
+
+fun main() {
+    Day15().solve()
+}
---
+++ src/test/kotlin/Day15Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class Day15Test {
+
+    @Test
+    fun part1() {
+        Assertions.assertEquals(1111, Day15().part1)
+    }
+
+    @Test
+    fun part2() {
+        Assertions.assertEquals(48568, Day15().part2)
+    }
+
+}
